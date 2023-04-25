ce8ae3bcc66b5fe8201dec091518e316133b8c9a
Kamoliddinin <newkama69@gmail.com>
2023-04-25 15:16:46 +0300

    added RegisterUser, LoginUser and logout_user in views.py

---
+++ sojournal/main/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+
+class RegisterUser(CreateView):
+    template_name = 'main/register.html'
+    form_class = UserCreationForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(LoginView):
+    template_name = 'main/register.html'
+    form_class = AuthenticationForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+@login_required()
+def logout_user(request):
+    logout(request)
+    return redirect('login')
